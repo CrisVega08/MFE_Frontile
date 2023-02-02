8c3af2fa277e303c932e17aba8987d15b749e88f
Cristian Vega <cristian.vega08@gmail.com>
2023-02-01 22:56:08 -0500

    landing working at port 8081

--- apps/landing/config/webpack.config.js
+++ apps/landing/config/webpack.config.js
@@ -1,1 +1,22 @@
 // webpack config
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8081
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'landing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './LandingApp': './src/index',
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
--- apps/landing/src/index.js
+++ apps/landing/src/index.js
@@ -1,1 +1,4 @@
 console.log('Landing running!!!')
+
+const element = document.querySelector('#root');
+element.innerHTML = '<h1> Landing Page working </h1>'
