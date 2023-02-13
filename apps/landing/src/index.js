0b21a6c37748641c141a2b7bc2fa886be23d76a7
Cristian Vega <cristian.vega08@gmail.com>
2023-02-13 10:25:59 -0500

    refactor for using only render functions

--- apps/landing/src/index.js
+++ apps/landing/src/index.js
@@ -1,4 +1,14 @@
 console.log('Landing running!!!')
 
-const element = document.querySelector('#root');
-element.innerHTML = '<h1> Landing Page working </h1>'
+const renderLanding = (el) => {
+  el.innerHTML = '<h1> Landing Page working </h1>'
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const rootElement = document.getElementById('landingApp');
+  if(rootElement) {
+    renderLanding(rootElement);
+  }
+}
+
+export { renderLanding };
